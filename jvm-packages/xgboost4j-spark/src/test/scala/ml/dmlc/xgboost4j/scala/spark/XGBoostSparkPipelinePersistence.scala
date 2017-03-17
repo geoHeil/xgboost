1b5a7ddd7d50c23f406fff2e61dcd10449405bf5
Georg Heiler <georg.kf.heiler@gmail.com>
2017-03-17 07:57:36 +0100

    Add example from https://github.com/dmlc/xgboost/issues/2115#issuecomment-287247577

--- jvm-packages/xgboost4j-spark/src/test/scala/ml/dmlc/xgboost4j/scala/spark/XGBoostSparkPipelinePersistence.scala
+++ jvm-packages/xgboost4j-spark/src/test/scala/ml/dmlc/xgboost4j/scala/spark/XGBoostSparkPipelinePersistence.scala
@@ -17,11 +17,15 @@
 package ml.dmlc.xgboost4j.scala.spark
 
 import org.apache.spark.SparkConf
+import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.feature._
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.sql.SparkSession
 
+import scala.collection.mutable
 import scala.concurrent.duration._
+import scala.util.Random
 
 case class Foobar(TARGET: Int, bar: Double, baz: Double)
 
@@ -96,6 +100,84 @@ class XGBoostSparkPipelinePersistence extends SharedSparkContext with Utils {
       === sameModelXGBParamMap.get(sameModelXGB.labelCol))
     assert(predictionModelXGBParamMap.get(predictionModelXGB.labelCol)
       === sameModelXGBParamMap.get(sameModelXGB.labelCol))
+  }
+
+  test("test sparks pipeline persistence of crossvalidation model") {
+    // xgboost parameters
+    def get_param(): mutable.HashMap[String, Any] = {
+      val params = new mutable.HashMap[String, Any]()
+      params += "eta" -> 0.1
+      params += "max_depth" -> 4
+      params += "min_child_weight" -> 4
+      params += "num_rounds" -> 20
+      params += "silent" -> 1
+      params += "objective" -> "binary:logistic"
+      params += "booster" -> "gbtree"
+      params += "gamma" -> 0.0
+      params += "colsample_bylevel" -> 1
+
+      return params
+    }
+
+    val r = new Random(0)
+    val conf: SparkConf = new SparkConf()
+      .setAppName("foo")
+      .setMaster("local[*]")
+
+    val spark: SparkSession = SparkSession
+      .builder()
+      .config(conf)
+      .getOrCreate()
+
+    val training = spark.createDataFrame(
+      Seq.fill(10000)(r.nextInt(2)).map(i => (i, i))
+    ).toDF("feature", "label")
+
+    val test = spark.createDataFrame(
+      Seq.fill(10000)(r.nextInt(2)).map(i => (i, i))
+    ).toDF("feature", "label")
+
+
+    // create pipeline
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("feature"))
+      .setOutputCol("features")
+
+    val xgb = new XGBoostEstimator(get_param().toMap)
+      .setFeaturesCol("features")
+
+    val pipeline = new Pipeline()
+      .setStages(Array(assembler, xgb))
+
+
+    // grid
+    val paramGrid = new ParamGridBuilder()
+      //  .addGrid(hashingTF.numFeatures, Array(10, 100, 1000))
+      .addGrid(xgb.round, Array(10, 30, 50, 100))
+      .addGrid(xgb.maxDepth, Array(5, 6, 8, 10))
+      .addGrid(xgb.minChildWeight, Array(0.5, 0.7, 1.0))
+      .build()
+    // cv
+    // val evaluator = new BinaryClassificationEvaluator().setRawPredictionCol("probabilities")
+    val evaluator = new RegressionEvaluator().setLabelCol("label")
+    val cv = new CrossValidator()
+      .setEstimator(pipeline)
+      .setEvaluator(new RegressionEvaluator()
+        .setLabelCol("label")
+        .setPredictionCol("prediction")
+        .setMetricName("rmse")
+      )
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(2)
+
+
+    // Run cross-validation, and choose the best set of parameters.
+    val cvModel = cv.fit(training)
+    val tempPath = TestUtils.createTempDir("cvModel")
+    cvModel.write.overwrite.save(tempPath)
+
+    // TODO complete meaningful test
+    assert(false, true)
   }
 }
 
